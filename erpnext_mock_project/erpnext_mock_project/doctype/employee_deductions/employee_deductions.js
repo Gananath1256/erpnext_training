90ba6b1f1a49f5ae3bc5826a5b198c2f65798d8f
Gananath1256 <gananathpasalapudi@gmail.com>
2022-10-08 11:36:51 +0530

    [mock project]

---
+++ erpnext_mock_project/erpnext_mock_project/doctype/employee_deductions/employee_deductions.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2022, erpnext_mock_project and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Deductions', {
+	refresh: function(frm) {
+
+	}
+});
+
+
+
+
+
+
+
+   // onload:function(frm,cdt,cdn){
+    //     console.log('console------------------------')
+    //     frm.set_query("confirm_customer",function(frm,cdt,cdn){
+    //         return{
+    //             filters:[
+    //                 ["Customer","customer_group","in","Individual"]
+    //             ]
+    //         }
---
+++ erpnext_mock_project/erpnext_mock_project/report/timesheet_details/timesheet_details.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2022, erpnext_mock_project and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+import frappe
+from frappe import _
+
+def execute(filters=None):
+	conditions = get_conditions(filters)
+	columns = get_columns()
+	data = get_data(filters,conditions)
+	return columns, data	
+def get_columns():
+	columns = [
+		{
+			"fieldname": "name",
+			"label":_("Employee"),
+			"fieldtype": "Data",
+			# "options": "Sales Order",
+			"width": 200
+		},
+		{
+			"fieldname": "employee_name",
+			"label": _("Full Name"),
+			"fieldtype": "Data",					
+			#"options": "Customer",
+			"width": 200
+		},
+		{
+			"fieldname": "status",
+			"label": _("Status"),
+			"fieldtype": "Select",
+			"options":("Present"),
+			"width": 200
+		},
+        {
+			"fieldname": "status",
+			"label": _("Status"),
+			"fieldtype": "Select",
+			"options":("Absent"),
+			"width": 200
+		},
+        {
+			"fieldname": "status",
+			"label": _("Status"),
+			"fieldtype": "Select",
+			"options":("On Leave"),
+			"width": 200
+		},
+        {
+			"fieldname": "status",
+			"label": _("Status"),
+			"fieldtype": "Select",
+			"options":("Half Day"),
+			"width": 200
+		},
+		{
+			"fieldname": "shift",
+			"label": _("Shift"),
+			"fieldtype": "Link",
+			"options": ("Shift Type"),
+			"width": 200	
+		},
+	
+	]
+	
+	return columns
+
+def get_data(filters,conditions):
+	data=frappe.db.sql(f"""select name,employee_name
+    from `tabAttendence`  
+	
+	'{filters.get('from_date')}' and '{filters.get('to_date')}' {conditions} """,as_dict=True)
+	return data 
+
+def get_conditions(filters):
+	condition    = ""
+	if filters.get('from_date'):
+		conditions += " AND  transaction_date > '{}'".format(filters.get('from_date'))
+	if filters.get('to_date'):
+		conditions+= " AND transaction_date <'{}'".format(filters.get('to_date'))
+	# if filters.get('status'):
+	# 	conditions+= " AND employee = '{}'".format(filters.get('employee'))
+	# return conditions
